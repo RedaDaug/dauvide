bf7665b1388468618307e0254394fac8d7468d8c
Reda <reda.daugelaviciute@gmail.com>
2020-11-19 21:50:28 +0100

    Apie mus & Kontaktai added

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Services\SuperiorProductService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,8 +14,35 @@ class MainController extends AbstractController
      */
     public function index(): Response
     {
-        return $this->render('main/index.html.twig', [
+        return $this->render('home/index.html.twig', [
             'controller_name' => 'MainController',
         ]);
     }
+
+    /**
+     * @Route("/kontaktai", name="kontaktai")
+     */
+
+    public function kontaktai(): Response
+    {
+        return $this->render('home/kontaktai.html.twig');
+    }
+
+    /**
+     * @Route("/apiemus", name="apiemus")
+     */
+
+    public function apiemus()
+    {
+        return $this->render('home/apiemus.html.twig');
+    }
+
+//    /**
+//     * @Route("/apiemus", name="apiemus")
+//     */
+//
+//    public function apiemus(): Response
+//    {
+//        return $this->render('home/apiemus.html.twig');
+//    }
 }

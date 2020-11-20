9ba95cc9402471e79e9e07520c91916b5c65898a
Reda <reda.daugelaviciute@gmail.com>
2020-11-20 15:59:01 +0100

    Brands and Categories Entities added

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller;
 
-use App\Services\SuperiorProductService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -37,12 +36,4 @@ class MainController extends AbstractController
         return $this->render('home/apiemus.html.twig');
     }
 
-//    /**
-//     * @Route("/apiemus", name="apiemus")
-//     */
-//
-//    public function apiemus(): Response
-//    {
-//        return $this->render('home/apiemus.html.twig');
-//    }
 }

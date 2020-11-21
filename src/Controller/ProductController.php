6f73d4904d9a21bf91fb6cb2ef86606d898d0bbc
Reda <reda.daugelaviciute@gmail.com>
2020-11-21 18:13:26 +0100

    Add new product page added

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Product;
+use App\Form\ProductType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -32,13 +33,17 @@ class ProductController extends AbstractController
     public function create(Request $request){
         $product = new Product();
 
-        $product->setName("AROMA gaiviklis")
+        $form = $this->createForm(ProductType::class, $product);
+        $form->handleRequest($request);
 
-            ->setImageName('visbella.png');
-        $em = $this->getDoctrine()->getManager();
-        $em->persist($product);
-        $em->flush();
-        return new Response("Gaiviklis pridetas");
+//        $em = $this->getDoctrine()->getManager();
+//        $em->persist($product);
+//        $em->flush();
+//        return new Response("Gaiviklis pridetas");
+
+        return $this->render('product/create.html.twig', [
+            'form' => $form->createView()
+        ]);
 
     }
 

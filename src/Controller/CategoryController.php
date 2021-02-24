e9560ab207ada8ae3fc048634ecd0ef335416e51
Reda <reda.daugelaviciute@gmail.com>
2021-02-24 17:57:31 +0100

    EcoVentis added

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -13,7 +13,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/category", name="category.")
+ * @Route("/kategorija", name="category.")
  */
 
 class CategoryController extends AbstractController

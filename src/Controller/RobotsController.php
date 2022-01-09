7e0df62f36de3134fd412bb8c0228be9b3bf30c2
Reda <reda.daugelaviciute@gmail.com>
2022-01-09 19:38:27 +0100

    robots.txt added

--- src/Controller/RobotsController.php
+++ src/Controller/RobotsController.php
@@ -6,17 +6,15 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class SitemapController extends AbstractController
+class RobotsController extends AbstractController
 {
     /**
-     * @Route("/sitemap.xml", name="sitemap")
+     * @Route("/robots.txt", name="robots")
      */
     public function index(): Response
     {
-        return $this->render('sitemap/index.html.twig', [
-            'controller_name' => 'SitemapController',
+        return $this->render('robots/index.html.twig', [
+            'controller_name' => 'RobotsController',
         ]);
     }
-
-
 }
